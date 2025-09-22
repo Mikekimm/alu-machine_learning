c3e33570f0a69a83b20dc804ee22523a920ab581
Mikekimm <mike4kimani@gmail.com>
2025-09-22 12:14:29 +0200

    Kimani

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
-""" Training with RMSProp (TensorFlow 2.x compatible)
+""" Training with momentum
 """
 
 import tensorflow as tf
 
-# Disable eager execution to use TF 1.x-style graph-based ops
-tf.compat.v1.disable_eager_execution()
-
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ creates the training operation for a neural network in tensorflow
+        using the RMSProp optimization algorithm
+        Args:
+            loss: is the loss of the network
+            alpha: is the learning rate
+            beta2: is the RMSProp weight
+            epsilon: is a small number to avoid division by zero
+        Returns: the RMSProp optimization operation
     """
-    Creates the training operation using RMSProp optimization algorithm.
 
-    Args:
-        loss: the loss tensor of the neural network
-        alpha: learning rate
-        beta2: RMSProp decay rate (like momentum)
-        epsilon: small number to avoid division by zero
-
-    Returns:
-        A training operation that minimizes the loss using RMSProp.
-    """
-    optimizer = tf.compat.v1.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
     return optimizer.minimize(loss)
-
