6d7ab738d96be1a0b15c9a075917c18b8ded2e45
Mikekimm <mike4kimani@gmail.com>
2025-09-28 19:40:30 +0200

    7777

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -24,17 +24,16 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         b_key = 'b{}'.format(l)
         W = weights[W_key]
 
-        # Gradients with L2 regularization
-        dW = (1 / m) * (np.matmul(dZ, A_prev.T) + lambtha * W)
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        # Compute gradients
+        dW = (np.matmul(dZ, A_prev.T) + lambtha * W) / m
+        db = np.sum(dZ, axis=1, keepdims=True) / m
 
-        # Update parameters
+        # Update weights and biases
         weights[W_key] -= alpha * dW
         weights[b_key] -= alpha * db
 
-        # Compute dZ for next layer (only if not the input layer)
+        # Backpropagate dZ to previous layer
         if l > 1:
             dA_prev = np.matmul(W.T, dZ)
             A_prev = cache['A{}'.format(l - 1)]
             dZ = dA_prev * (1 - A_prev ** 2)  # tanh derivative
-
