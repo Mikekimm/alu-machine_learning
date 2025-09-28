56cfc5e4be65903df467c1cef3fe2faff0d602c0
Mikekimm <mike4kimani@gmail.com>
2025-09-28 19:32:38 +0200

    999

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """Calculates the cost of a neural network with L2 regularization
+
+    Args:
+        cost: cost without regularization
+        lambtha: regularization parameter
+        weights: dict of weights and biases
+        L: number of layers
+        m: number of data points
+
+    Returns:
+        Total cost with L2 regularization
+    """
+    l2_sum = 0
+    for l in range(1, L + 1):
+        W = weights['W{}'.format(l)]
+        l2_sum += np.sum(np.square(W))
+
+    l2_cost = cost + (lambtha / (2 * m)) * l2_sum
+    return l2_cost
+
