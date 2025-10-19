0489f9fab528e7898ab069dd26dd6d8a9cb33c52
Mikekimm <mike4kimani@gmail.com>
2025-10-19 22:44:40 +0200

    Mikekim

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
-"""
-Test file
-"""
-availableShips = __import__('0-passengers').availableShips
+""" Return list of ships"""
 
-ships = availableShips(4)
-for ship in ships:
-    print(ship)
 
+import requests
+
+
+def availableShips(passengerCount):
+    """ Return list of ships
+
+    Args:
+        passengerCount (int): number of ships
+    """
+
+    res = requests.get('https://swapi-api.alx-tools.com/api/starships')
+
+    output = []
+    while res.status_code == 200:
+        res = res.json()
+        for ship in res['results']:
+            passengers = ship['passengers'].replace(',', '')
+            try:
+                if int(passengers) >= passengerCount:
+                    output.append(ship['name'])
+            except ValueError:
+                pass
+        try:
+            res = requests.get(res['next'])
+        except Exception:
+            break
+    return output
