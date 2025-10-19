29a169d82dcbf713b99e9eb187cc96933741267a
Mikekimm <mike4kimani@gmail.com>
2025-10-19 22:46:32 +0200

    BKA

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python3
-"""Return user's location based on URL response."""
+
+
+""" Return list of ships"""
 
 import requests
 import sys
 import time
 
 
-def get_user_location(url):
-    """Handles response and prints location or appropriate status."""
-    try:
-        res = requests.get(url)
-    except requests.RequestException:
-        print("Request failed")
-        return
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
 
     if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
         current_time = int(time.time())
-        diff = (rate_limit - current_time) / 60
-        print("Rate limit. Retry in {} min".format(int(round(diff))))
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
     elif res.status_code == 404:
         print("Not found")
     elif res.status_code == 200:
-        data = res.json()
-        print(data.get('location'))
-    else:
-        print("Unexpected status code:", res.status_code)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./script.py <URL>")
-        sys.exit(1)
-
-    get_user_location(sys.argv[1])
+        res = res.json()
+        print(res['location'])
