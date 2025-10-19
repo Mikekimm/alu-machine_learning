fdc61ed2f24afe0083c9ececaff888d5194dd08e
Mikekimm <mike4kimani@gmail.com>
2025-10-19 22:29:44 +0200

    mike

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
-
-""" Return list of ships"""
+"""Return user's location based on URL response."""
 
 import requests
 import sys
 import time
 
 
-if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
+def get_user_location(url):
+    """Handles response and prints location or appropriate status."""
+    try:
+        res = requests.get(url)
+    except requests.RequestException:
+        print("Request failed")
+        return
 
     if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
         current_time = int(time.time())
         diff = int((rate_limit - current_time) / 60)
-        print(f"Rest in {diff} min")
-        # get remaining rate
-
+        print("Rest in {} min".format(diff))
     elif res.status_code == 404:
         print("Not found")
     elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+        data = res.json()
+        print(data.get('location'))
+    else:
+        print("Unexpected status code:", res.status_code)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./script.py <URL>")
+        sys.exit(1)
+
+    get_user_location(sys.argv[1])
